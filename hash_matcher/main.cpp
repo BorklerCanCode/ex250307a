2f7e391b0c92447217edba6013c14ee2852eeb15
BorklerCanCode <jasminebourdlaies@gmail.com>
2025-03-12 13:15:00 -0700

    clerical commit 20250312-1315.00

--- hash_matcher/main.cpp
+++ hash_matcher/main.cpp
@@ -2,12 +2,16 @@
 #include <iostream>
 
 int main() {
+    //Detect platform
     #ifdef __aarch64__
         std::cout << "ARM64 platform detected" << std::endl;
+        //assign arch=arm64
     #elif __x86_64__
         std::cout << "x86_64 platform detected" << std::endl;
+        //assign arch=x86_64
     #else
-        std::cout << "WARNING: Uknown architecture detected." << std::endl;
+        std::cout << "ERROR: Uknown architecture detected." << std::endl;
+        //exit upon encountering unknown platform, return coded error.
         return 10011;
     #endif
     return 0;
